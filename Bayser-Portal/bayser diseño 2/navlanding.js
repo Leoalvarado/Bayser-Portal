fbb65407bb88fe513c451579786911d35a10744d
Willard19 <60764938+Willard19@users.noreply.github.com>
2020-04-09 15:46:26 -0600

    Update navlanding.js

--- Bayser-Portal/bayser diseño 2/navlanding.js
+++ Bayser-Portal/bayser diseño 2/navlanding.js
@@ -14,13 +14,13 @@ $(document).ready(function(){
 				$("header nav ul li a").css({"color": "white"});
 				$("ul li ul li a").css({"color": "white"});
 				
-				$("ul li ul").css({"background-color": "rgba(20, 19, 19, 0.68)"});
+				/*$("ul li ul").css({"background-color": "rgba(20, 19, 19, 0.68)"});*/
 				flag = true;
 			}
 		}else{
 			if(flag){
 				$("nav").css({"background-color": "transparent"});
-				$("ul li ul").css({"background-color": "rgba(255, 254, 254, 0.596)"});
+				/*$("ul li ul").css({"background-color": "rgba(255, 254, 254, 0.596)"});*/
 				$("header nav ul li a").css({"color": "#333333"});
 				$("ul li ul li a").css({"color": "#333333"});
 				flag = false;
